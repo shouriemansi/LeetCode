8b16ae6ec9017f1b93d1eb971cfd1e4e588f7bd0
Mansi Shourie <mansi.shourie24@gmail.com>
2026-08-31 12:37:29 +0530

    Time: 18 ms (77.6%), Space: 107.8 MB (66.69%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> num=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(num.contains(nums[i])){
+                return true;
+            }
+            num.add(nums[i]);
+        }
+        return false;
+    }
+}
